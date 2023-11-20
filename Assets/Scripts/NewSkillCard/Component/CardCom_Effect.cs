79626f99993dd2fcfd715ad992321cc52c14cc19
LinWenHao <1095322545@qq.com>
2023-11-20 18:57:01 +0800

    4

--- Assets/Scripts/NewSkillCard/Component/CardCom_Effect.cs
+++ Assets/Scripts/NewSkillCard/Component/CardCom_Effect.cs
@@ -1,5 +1,7 @@
-﻿using Dreamteck.Splines;
+﻿using System;
+using Dreamteck.Splines;
 using HighlightPlus;
+using UnityEngine;
 
 namespace NewSkillCard {
     public class CardCom_Effect : BaseComponent<CardCtrl> {
@@ -33,7 +35,22 @@ namespace NewSkillCard {
             HighlightEffectRef.SetHighlighted(true);
         }
 
+        public void Update() {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+            var pos1          = ComOwner.transform.position;
+            var pos3          = mouseWorldPos;
+            var pos2          = new Vector3(pos1.x, pos3.y, pos3.z);
+
+            var oldPoints = SplineComputerRef.GetPoints();
+            oldPoints[0].position = pos1;
+            oldPoints[1].position = pos2;
+            oldPoints[2].position = pos3;
+            SplineComputerRef.SetPoints(oldPoints);
+        }
+
         public void ArrowFollowMouse() {
+            SplineComputerRef.enabled = true;
+            SplineRendererRef.enabled = true;
             SplineComputerRef.EvaluatePositions();
         }
     }
