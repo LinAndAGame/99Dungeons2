1cd7c40009c3078f90bb02a0a7c9a1ea2b51ad7d
LinWenHao <1095322545@qq.com>
2023-11-29 14:24:51 +0800

    2

---
+++ Assets/Scripts/NewRole/Component/RoleCom_Model.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace NewRole {
+    public class RoleCom_Model : BaseComponent<RoleCtrl> {
+        public SpriteRenderer SR_Role;
+        
+        public override void Init(RoleCtrl comOwner) {
+            base.Init(comOwner);
+            SR_Role.sprite = ComOwner.RuntimeDataRole.SaveData.AssetData.SpriteRole;
+        }
+    }
+}
